8849e273931933ae06da5879e6e5853ab6ad7e21
guilherme <guilherme@inovadora.com.br>
2023-08-03 11:41:36 -0300

    Adicionado servico de bloqueio e desbloqueio de leito. refs #83591

---
+++ lib/gerint/servicos/desbloqueio_leito.rb
@@ -0,0 +1,32 @@
+
+module Gerint
+    
+    class DesbloqueioLeito < Gerint::Padrao
+        attr_accessor :identLeito, :codTipoLeito
+
+        def as_json(options={})
+            h = {
+                identLeito: @identLeito, 
+                codTipoLeito: @codTipoLeito
+            }
+            h
+        end
+
+        def all_json # Gera hash com todas as variaveis de instancia
+            vinstancia = self.instance_variables  
+            h = {}
+            vinstancia.each do |v|
+                h.store(v[1..50], "#{v}") # BUG: v contem o nome da variavel e não o valor int
+            end
+            h
+            h.to_json
+        end
+        
+        def to_json(*options)
+            as_json(*options).to_json(*options)
+        end
+ 
+    end
+    
+end 
+
